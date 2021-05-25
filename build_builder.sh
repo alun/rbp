6ec257116a1f8553e26624bfd16ad14793c95f76
Alex Lunacharskii <alun@katlex.com>
2021-05-25 20:35:46 +0100

    Trying CI one more time

--- build_builder.sh
+++ build_builder.sh
@@ -1,3 +1,4 @@
+set -e
 docker build builder -t rbp-rust-builder
 docker tag rbp-rust-builder registry.gitlab.com/katlex/github-runner:rbp-rust-builder
 docker push registry.gitlab.com/katlex/github-runner:rbp-rust-builder
